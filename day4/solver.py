e8242b661e24a94f526c0c7f9333f074117e0abb
Remi Paulmier <remi.paulmier@gmail.com>
2019-12-04 11:53:05 +0100

    day4#2

--- day4/solver.py
+++ day4/solver.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
 
+from itertools import groupby
+
 candidates = []
 
 # cond. 1-2
-for n in range(136818, 685979):
+for n in range(136818, 685979+1):
     s = str(n)
 
-    # cond. 3: 2 adjacent digits are the same
-    candidate = False
-    for i in range(5):
-        if s[i] == s[i+1]:
-            candidate = True
-            break
-    if not candidate:
+    # cond. 3&5: 2 and only 2 adjacent digits are the same
+    groups = [digit for digit, group in groupby(s) if len(list(group)) == 2]
+    if len(groups) == 0:
         continue
 
-    # cond. 4: increasing digits:
+    # cond. 4: increasing digits
     if ''.join(sorted(s)) != s:
         continue
 
-
     candidates.append(n)
+
 
 print(len(candidates))
