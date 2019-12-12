df172adf16c6d31cbae3adddf6b44da6a6f0d197
Remi Paulmier <remi.paulmier@gmail.com>
2019-12-13 00:21:33 +0100

    day7#2

--- day7/solver.py
+++ day7/solver.py
@@ -3,6 +3,7 @@
 from enum import Enum
 from copy import copy
 from itertools import permutations
+from multiprocessing import Process, Pipe
 
 import click
 
@@ -84,14 +85,14 @@ def op_multiplication(ctx, pointer):
 
 def op_input(ctx, pointer):
     address, pointer = read_parameters(ctx, pointer, False)
-    user_input = ctx['stdin'].pop()
+    user_input = ctx['stdin'].recv()
     ctx['memory'][address] = user_input
     return pointer
 
 
 def op_output(ctx, pointer):
     p1, pointer = read_parameters(ctx, pointer, True)
-    ctx['stdout'].append(p1)
+    ctx['stdout'].send(p1)
     return pointer
 
 
@@ -139,8 +140,8 @@ def run_instruction(ctx, pointer):
     return pointer
 
 
-def run(program, phase, sigin):
-    ctx = dict(memory=copy(program), stdin=[sigin, phase], stdout=[])
+def run(program, stdin, stdout):
+    ctx = dict(memory=copy(program), stdin=stdin, stdout=stdout)
     pointer = 0
 
     try:
@@ -154,26 +155,49 @@ def run(program, phase, sigin):
     except UnknownModeError:
         click.secho(f'Unknown mode: {ctx["memory"][pointer]} at position {pointer}:', fg='red')
 
-    return ctx['stdout'].pop()
-
 
 def get_thrusters_power(phase_seq):
+    seq_len = len(phase_seq)
     acs = load_program()
-    sigout = 0
+    # acs = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
+    # acs = [3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]
+
+    # create crossed pipes from one process to previous
+    stdios = {phase: ['stdin', 'stdout'] for phase in phase_seq}
+    for i in range(seq_len):
+        stdios[phase_seq[i]][0], stdios[phase_seq[i-1]][1] = Pipe()
+
+    processes = {}
     for phase in phase_seq:
-        sigout = run(acs, phase=phase, sigin=sigout)
-    return sigout
+        processes[phase] = Process(target=run, args=(acs, stdios[phase][0], stdios[phase][1]))
+        processes[phase].start()
+
+    # send phase of each process to stdout of previous process
+    for i in range(seq_len):
+        stdios[phase_seq[i-1]][1].send(phase_seq[i])
+
+    # send initial signal to stdout of last process
+    stdios[phase_seq[-1]][1].send(0)
+
+    # feedback loop
+    while True:
+        # check if last process is halted
+        processes[phase_seq[-1]].join(timeout=1)
+        if processes[phase_seq[-1]].exitcode is not None:
+            break
+
+    # read last outpout from stdin of first process
+    return stdios[phase_seq[0]][0].recv()
 
 
 def main():
     max_power = 0
 
-    for phase_seq in permutations([0, 1, 2, 3, 4]):
+    for phase_seq in permutations([5, 6, 7, 8, 9]):
         power = get_thrusters_power(phase_seq)
         if power > max_power:
             click.secho(f'{power} is greater than {max_power}, pivoting.', fg='green')
             max_power = power
-
     click.secho(f'Max power = {max_power}', fg='magenta')
 
 
